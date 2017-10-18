c96f3bc5b624434b602127ba3595d3150a22fe00
Ryan Kaplan <ryan_kaplan@me.com>
2017-10-18 13:31:33 -0700

    Adds function to gather all dataset.language attributes of selected tags and passes them to array on the "Add" button click. #4

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   var addButton = document.getElementById('add-button')
   addButton.addEventListener('click', function () {
     chrome.tabs.query({
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }, tabs => {
       const tab = tabs[0]
       let url = tab.url
+      getSelectedTags()
+      console.log(selectedTags)
       fetch('http://localhost:3000/api/pages', {
         method: 'POST',
         body: '{"url":  "' + url + '" }',
@@ -15,91 +17,27 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error(err)
       })
     })
+    selectedTags = []
   })
-})
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const html = document.getElementById('html')
-//   html.addEventListener('click', () => {
-//     if (html.classList.contains('grey')) {
-//       html.classList.add('orange', 'darken-3')
-//       html.classList.remove('grey')
-//     }
-//     else {
-//       html.classList.add('grey')
-//       html.classList.remove('orange', 'darken-3')
-//     }
-//   })
-//   const css = document.getElementById('css')
-//   css.addEventListener('click', () => {
-//     css.classList.add('light-blue')
-//     css.classList.remove('grey')
-//   })
-//   const javascript = document.getElementById('javascript')
-//   javascript.addEventListener('click', () => {
-//     javascript.classList.add('yellow')
-//     javascript.classList.remove('grey')
-//   })
-//   const nodejs = document.getElementById('nodejs')
-//   nodejs.addEventListener('click', () => {
-//     nodejs.classList.add('light-green', 'darken-1')
-//     nodejs.classList.remove('grey')
-//   })
-//   const express = document.getElementById('express')
-//   express.addEventListener('click', () => {
-//     express.classList.add('lighten-1')
-//   })
-//   const angularjs = document.getElementById('angularjs')
-//   angularjs.addEventListener('click', () => {
-//     angularjs.classList.add('red', 'darken-1')
-//     angularjs.classList.remove('grey')
-//   })
-//   const react = document.getElementById('react')
-//   react.addEventListener('click', () => {
-//     react.classList.add('blue', 'lighten-1')
-//     react.classList.remove('grey')
-//   })
-//   const mongodb = document.getElementById('mongodb')
-//   mongodb.addEventListener('click', () => {
-//     mongodb.classList.add('green', 'darken-1')
-//     mongodb.classList.remove('grey')
-//   })
-//   const bootstrap = document.getElementById('bootstrap')
-//   bootstrap.addEventListener('click', () => {
-//     bootstrap.classList.add('deep-purple')
-//     bootstrap.classList.remove('grey')
-//   })
-//   const git = document.getElementById('git')
-//   git.addEventListener('click', () => {
-//     git.classList.add('orange', 'darken-4')
-//     git.classList.remove('grey')
-//   })
-//   const heroku = document.getElementById('heroku')
-//   heroku.addEventListener('click', () => {
-//     heroku.classList.add('deep-purple', 'lighten-3')
-//     heroku.classList.remove('grey')
-//   })
-// })
-
-document.addEventListener('DOMContentLoaded', () => {
-  const tags = document.querySelectorAll('.hoverable')
-  const tagsToAdd = []
+  const tags = document.querySelectorAll('.tags')
   for (let i = 0; i < tags.length; i++) {
     tags[i].addEventListener('click', function () {
       tags[i].classList.toggle('grey')
       tags[i].classList.toggle('red')
-      tagsToAdd.push(tags[i].dataset.test)
-      console.log(tagsToAdd)
-    }, false)
+      tags[i].classList.toggle('clicked')
+    })
   }
-// If element already exists, remove it
 })
 
-// $('.sal').each(function(elem) {
-//             $(this).click(function(e) {
-//
-//                  i.push($(this).val());
-
+let selectedTags = []
+const getSelectedTags = () => {
+  const tags = document.querySelectorAll('.tags')
+  tags.forEach(element => {
+    if (element.classList.contains('clicked')) {
+      selectedTags.push(element.dataset.language)
+    }
+  })
+}
 // Listen to the row for clicks.
 // Determine if the clicked thing was icon.
 // If not, return
