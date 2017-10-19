00fb37c88239a5ec5d4039aed5ea3e18fb293e26
Ryan Kaplan <ryan_kaplan@me.com>
2017-10-19 13:28:29 -0700

    Adds additional seed data for test cases (continued).

--- seed.js
+++ seed.js
@@ -31,8 +31,7 @@ MongoClient.connect('mongodb://localhost/updoot', (err, db) => {
       'image': 'https://three29.com/wp-content/uploads/2017/03/Contstruction-e1490997182922.jpg',
       'publisher': 'Three29',
       tags: [
-        'heroku',
-        'nodejs'
+        'css'
       ],
       'title': 'Best CSS Frameworks of 2017 | Three29',
       'url': 'https://three29.com/best-css-frameworks-2017/',
@@ -45,8 +44,7 @@ MongoClient.connect('mongodb://localhost/updoot', (err, db) => {
       image: 'https://cdn.filestackcontent.com/rXw89KfCQIuvWal613HB',
       publisher: 'Codementor',
       tags: [
-        'html',
-        'git'
+        'express'
       ],
       title: 'Creating a simple REST API with expressJS in 5min | Codementor',
       url: 'https://www.codementor.io/wapjude/creating-a-simple-rest-api-with-expressjs-in-5min-bbtmk51mq',
@@ -92,6 +90,91 @@ MongoClient.connect('mongodb://localhost/updoot', (err, db) => {
       ],
       'title': 'AngularJS Fundamentals - Course by @simpulton',
       'url': 'https://egghead.io/courses/angularjs-fundamentals',
+      'vote': 1
+    },
+    {
+      '_id': '59e907f7a63d8c668983d905',
+      'author': 'Atlassian',
+      'date': null,
+      'description': 'Learn Git with beginner to advanced Git tutorials on workflows, git commands, git migration and code review.',
+      'image': 'https://wac-cdn.atlassian.com/dam/jcr:c1326bc1-cede-4e55-b793-581f8370de17/hero%20(1).svg?cdnVersion=ht',
+      'publisher': 'Atlassian',
+      'tags': [
+        'git'
+      ],
+      'title': 'Learn Git- Git tutorials, workflows and commands | Atlassian Git Tutorial',
+      'url': 'https://www.atlassian.com/git',
+      'vote': 1
+    },
+    {
+      '_id': '59e9086e8aa88666a5cfc9ae',
+      'author': null,
+      'date': null,
+      'description': 'How to Use Git and Github focuses on the use of version control with Git, a specific version control system, and GitHub, a collaboration platform.',
+      'image': 'https://s3-us-west-1.amazonaws.com/udacity-content/course/images/ud775-b28dc35.jpg',
+      'publisher': 'Udacity',
+      'tags': [
+        'git'
+      ],
+      'title': 'How to Use Version Control in Git & GitHub | Udacity',
+      'url': 'https://www.udacity.com/course/how-to-use-git-and-github--ud775',
+      'vote': 1
+    },
+    {
+      '_id': '59e908c2c2483266bf48baee',
+      'author': null,
+      'date': null,
+      'description': 'A step-by-step guide for deploying your first Node app and mastering the basics of Heroku',
+      'image': 'https://www.herokucdn.com/images/og.png',
+      'publisher': 'Heroku Dev Center',
+      'tags': [
+        'heroku'
+      ],
+      'title': 'Getting Started on Heroku with Node.js | Heroku Dev Center',
+      'url': 'https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up',
+      'vote': 1
+    },
+    {
+      '_id': '59e9092c8013d966dc85aae0',
+      'author': null,
+      'date': null,
+      'description': 'Websites using Bootstrap. Best examples of websites made with Bootstrap',
+      'image': 'https://assets.awwwards.com/bundles/tvweb/images/share/awwwards.jpg',
+      'publisher': 'Web Design Inspiration',
+      'tags': [
+        'bootstrap'
+      ],
+      'title': 'Best Bootstrap Websites | Web Design Inspiration',
+      'url': 'https://www.awwwards.com/websites/bootstrap/',
+      'vote': 1
+    },
+    {
+      '_id': '59e9097d754dc566f359b7ce',
+      'author': null,
+      'date': null,
+      'description': 'This is the second in a series of blog posts examining the technologies that are driving the development of modern web and mobile applications. This post specifically demonstrates how to use MongoDB from Node.js.',
+      'image': 'https://webassets.mongodb.com/_com_assets/cms/mongodb-for-giant-ideas-bbab5c3cf8.png',
+      'publisher': 'MongoDB',
+      'tags': [
+        'nodejs',
+        'mongodb'
+      ],
+      'title': 'Using MongoDB With Node.js',
+      'url': 'https://www.mongodb.com/blog/post/the-modern-application-stack-part-2-using-mongodb-with-nodejs',
+      'vote': 1
+    },
+    {
+      '_id': '59e909f2ac7b02670d2a3b38',
+      'author': null,
+      'date': null,
+      'description': 'Know what to ask. Know how to answer.',
+      'image': 'https://assets.toptal.io/assets/front/static/blog/blocks/interview_questions/illustration_3af347.png',
+      'publisher': 'Toptal',
+      'tags': [
+        'javascript'
+      ],
+      'title': 'Essential JavaScript Interview Questions',
+      'url': 'https://www.toptal.com/javascript/interview-questions',
       'vote': 1
     }
     ]))
